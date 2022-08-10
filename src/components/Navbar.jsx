c905e77f4062d748723c185ad33b41389cd78a92
shahin <shahin.shahi1986@gmail.com>
2022-08-10 19:40:53 +0300

    make route of auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-            <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Netflix</h1>
-            <button className='text-white pr-4'>Sing in</button>
-            <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white '>Sing in</button>
+            <Link to="/">
+                <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Netflix</h1>
+            </Link>
+            <Link to='/login'>
+                <button className='text-white pr-4'>Sing in</button>
+            </Link>
+            <Link to='/signup'>
+                <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white '>Sing in</button>
+            </Link>
         </div>
     )
 }
