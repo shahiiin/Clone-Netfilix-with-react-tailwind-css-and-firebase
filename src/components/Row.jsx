e6bc3c2a4b455a98d381e9595d9e87fe4bc433d7
shahin <shahin.shahi1986@gmail.com>
2022-08-10 17:31:11 +0300

    make style of sidebar

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -19,12 +19,11 @@ const Row = ({ title, fetchURL }) => {
             <div className="relative flex items-center">
                 <div id={'slider'}>
                     {movies.map((item, id) => (
-                        <div key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer p-3'>
-                           <img src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-                           
-                           <div>
-
-                           </div>
+                        <div key={id} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer p-2 relative'>
+                            <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
+                            <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 hover:opacity-100 opacity-0 text-white'>
+                                <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
+                            </div>
                         </div>
                     ))}
                 </div>
