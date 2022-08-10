c95f0064604e8c34155e559ca52f131579acc925
shahin <shahin.shahi1986@gmail.com>
2022-08-10 11:05:11 +0300

    call api from movie database

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import request from "../Request"
+
+
 const Main = () => {
 
     const [movies, setMovies] = useState([])
 
+    const movie = movies[Math.floor(Math.random() * movies.length)]
+
+    useEffect(() => {
+        axios.get(request.requestPopular).then((response) => {
+            setMovies(response.data.results)
+        })
+    }, [])
+    console.log(movie, 'aa')
 
     return (
         <div>Main</div>
