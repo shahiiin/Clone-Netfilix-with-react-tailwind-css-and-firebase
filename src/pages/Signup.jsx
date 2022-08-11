7bb19fcba7b7d4d84a1a614aacb6915d64c03c42
shahin <shahin.shahi1986@gmail.com>
2022-08-11 15:40:13 +0300

    start make signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <>
+            <div className='w-full h-screen'>
+                <img src="https://assets.nflxext.com/ffe/siteui/vlv3/a1543997-c1fd-4946-92d3-b0a3648b92c7/f602d484-99ee-4a68-a318-669bdff99948/TR-en-20220808-popsignuptwoweeks-perspective_alpha_website_medium.jpg" alt="/" />
+            </div>
+        </>
+
+    )
 }
 
 export default Signup
